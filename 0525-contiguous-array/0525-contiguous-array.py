7e5526240607c1adab13e918526b4ef419be684b
Yeabisera Tesefaye <117275171+YeabTesfaye@users.noreply.github.com>
2024-12-26 12:30:36 +0300

    Time: 99 ms (79.91%), Space: 23.6 MB (9.71%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,11 +1,10 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-             # Initialize the hashmap to store the first occurrence of prefix sum
-        prefix_sum_map = {0: -1}  # We assume prefix sum 0 occurs at index -1 for subarrays that start from the beginning
+        # Initialize the hashmap to store the first occurrence of prefix sum
+        prefix_sum_map = {0:-1}  
         max_length = 0
         prefix_sum = 0
 
-        # Iterate through the array
         for i, num in enumerate(nums):
             # Update the prefix sum: treat 0 as -1 and 1 as +1
             prefix_sum += 1 if num == 1 else -1
@@ -16,7 +15,6 @@ class Solution:
                 length = i - prefix_sum_map[prefix_sum]
                 max_length = max(max_length, length)
             else:
-                # Store the first occurrence of this prefix sum
                 prefix_sum_map[prefix_sum] = i
 
         return max_length
